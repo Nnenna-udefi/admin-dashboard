0135e645aba18942a8b5513bf5b456c22cad1cbe
Nnenna-udefi <miriamudefi@gmail.com>
2022-05-18 13:10:15 -0700

    fetching api

--- src/components/Dashboard/Clients/client.js
+++ src/components/Dashboard/Clients/client.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Dashboard from '../../pages/dashboard'
-import Axios from 'axios'
+// import Axios from 'axios'
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+// import { useParams } from 'react-router-dom'
 
 const Client = () => {
   // const url = 'https://fakerapi.it/api/v1/companies?_quantity=1'
@@ -17,59 +17,131 @@ const Client = () => {
 
   // }, [url])
   
- const {id} = useParams()
-  const [clients, setClients] = useState(null)
+ 
+  const [clients, setClients] = useState([])
 
-  let content = null
+  useEffect(() => {
+    fetch('https://fakerapi.it/api/v1/persons?_quantity=10')
+  .then(res => res.json())
+  .then(data => setClients(data.data))
 
-  useEffect(() => { 
-    fetch('https://fakerapi.it/api/v1/companies?_quantity=1')
-    .then(response => response.json)
-    .then(data => setClients(data.data))
+
   
     
   }, [])
+
   console.log(clients)
 
-  if(clients){
-    content =  
-    
-    <div>
+  
+  return (
+    <div className='main'>
+      <Dashboard />
+      <div>
+
+        <h2>Our Clients</h2>
     <table>
           <thead>
-              <tr>
-                  <th>FULL NAME</th>
+          <tr>
+            <th></th>
+                  <th>FIRST NAME</th>
+                  <th>LAST NAME</th>
                   <th>EMAIL</th>
                   <th>PHONE NUMBER</th>
-                  <th>GENDER</th>
+                  
               </tr>
           </thead>
           
 
           <tbody>
-            {
+            
           
-              <tr >
-                <td>{clients.name}</td>
-              <td>{clients.email}</td>
-              <td>{clients.phone}</td>
-              <td>{clients.gender}</td>
-             
+              <tr>
+                <td>{clients[0].id}</td>
+                <td>{clients[0].firstname}</td>
+                <td>{clients[0].lastname}</td>
+              <td>{clients[0].email}</td>
+              <td>{clients[0].phone}</td>
+            </tr>
+
+            
+            <tr >
+            <td>{clients[1].id}</td>
+                <td>{clients[1].firstname}</td>
+                <td>{clients[1].lastname}</td>
+              <td>{clients[1].email}</td>
+              <td>{clients[1].phone}</td>
+            </tr>
+
+            
+            <tr >
+            <td>{clients[2].id}</td>
+                <td>{clients[2].firstname}</td>
+                <td>{clients[2].lastname}</td>
+              <td>{clients[2].email}</td>
+              <td>{clients[2].phone}</td>
+            </tr>
+
+            <tr >
+            <td>{clients[3].id}</td>
+                <td>{clients[3].firstname}</td>
+                <td>{clients[3].lastname}</td>
+              <td>{clients[3].email}</td>
+              <td>{clients[3].phone}</td>
+            </tr>
+
+            <tr >
+            <td>{clients[4].id}</td>
+                <td>{clients[4].firstname}</td>
+                <td>{clients[4].lastname}</td>
+              <td>{clients[4].email}</td>
+              <td>{clients[4].phone}</td>
+            </tr>
+
+            <tr >
+            <td>{clients[5].id}</td>
+                <td>{clients[5].firstname}</td>
+                <td>{clients[5].lastname}</td>
+              <td>{clients[5].email}</td>
+              <td>{clients[5].phone}</td>
+            </tr>
+
+            <tr >
+            <td>{clients[6].id}</td>
+                <td>{clients[6].firstname}</td>
+                <td>{clients[6].lastname}</td>
+              <td>{clients[6].email}</td>
+              <td>{clients[6].phone}</td>
+            </tr>
+
+            <tr >
+            <td>{clients[7].id}</td>
+                <td>{clients[7].firstname}</td>
+                <td>{clients[7].lastname}</td>
+              <td>{clients[7].email}</td>
+              <td>{clients[7].phone}</td>
+            </tr>
+
+            <tr >
+            <td>{clients[8].id}</td>
+                <td>{clients[8].firstname}</td>
+                <td>{clients[8].lastname}</td>
+              <td>{clients[8].email}</td>
+              <td>{clients[8].phone}</td>
+            </tr>
+
+            <tr >
+            <td>{clients[9].id}</td>
+                <td>{clients[9].firstname}</td>
+                <td>{clients[9].lastname}</td>
+              <td>{clients[9].email}</td>
+              <td>{clients[9].phone}</td>
             </tr>
          
 
-            }
-            
           </tbody>
        </table>
     </div>
 
-  }
-  
-  return (
-    <div className='main'>
-      <Dashboard />
-      {content}
 
     </div>
   )
--- src/components/Dashboard/Members/member.js
+++ src/components/Dashboard/Members/member.js
@@ -8,7 +8,7 @@ const Member = () => {
 
   useEffect(() => {
     fetch('https://fakerapi.it/api/v1/persons?_quantity=10')
-  .then(response => response.json())
+  .then(res => res.json())
   .then(data => setMember(data.data))
 
 
@@ -22,31 +22,98 @@ const Member = () => {
   return (
     <div className='main'>
         <Dashboard />
+
+        <h2>Team Memebers</h2>
    
         <table>
           <thead>
               <tr>
-                  <th>FULL NAME</th>
+                  <th>FIRST NAME</th>
+                  <th>LAST NAME</th>
                   <th>EMAIL</th>
                   <th>PHONE NUMBER</th>
-                  <th>GENDER</th>
+                  
               </tr>
           </thead>
           
 
           <tbody>
-            {
+            
           
               <tr >
-                <td>{member.name}</td>
-              <td>{member.email}</td>
-              <td>{member.phone}</td>
-              <td>{member.gender}</td>
-             
+                <td>{member[0].firstname}</td>
+                <td>{member[0].lastname}</td>
+              <td>{member[0].email}</td>
+              <td>{member[0].phone}</td>
+            </tr>
+
+            
+            <tr >
+                <td>{member[1].firstname}</td>
+                <td>{member[1].lastname}</td>
+              <td>{member[1].email}</td>
+              <td>{member[1].phone}</td>
+            </tr>
+
+            
+            <tr >
+                <td>{member[2].firstname}</td>
+                <td>{member[2].lastname}</td>
+              <td>{member[2].email}</td>
+              <td>{member[2].phone}</td>
+            </tr>
+
+            <tr >
+                <td>{member[3].firstname}</td>
+                <td>{member[3].lastname}</td>
+              <td>{member[3].email}</td>
+              <td>{member[3].phone}</td>
+            </tr>
+
+            <tr >
+                <td>{member[4].firstname}</td>
+                <td>{member[4].lastname}</td>
+              <td>{member[4].email}</td>
+              <td>{member[4].phone}</td>
+            </tr>
+
+            <tr >
+                <td>{member[5].firstname}</td>
+                <td>{member[5].lastname}</td>
+              <td>{member[5].email}</td>
+              <td>{member[5].phone}</td>
+            </tr>
+
+            <tr >
+                <td>{member[6].firstname}</td>
+                <td>{member[6].lastname}</td>
+              <td>{member[6].email}</td>
+              <td>{member[6].phone}</td>
+            </tr>
+
+            <tr >
+                <td>{member[7].firstname}</td>
+                <td>{member[7].lastname}</td>
+              <td>{member[7].email}</td>
+              <td>{member[7].phone}</td>
+            </tr>
+
+            <tr >
+                <td>{member[8].firstname}</td>
+                <td>{member[8].lastname}</td>
+              <td>{member[8].email}</td>
+              <td>{member[8].phone}</td>
+            </tr>
+
+            <tr >
+                <td>{member[9].firstname}</td>
+                <td>{member[9].lastname}</td>
+              <td>{member[9].email}</td>
+              <td>{member[9].phone}</td>
             </tr>
          
 
-            }
+            
             
           </tbody>
        </table>
