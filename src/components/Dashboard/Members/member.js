1eb24b1b533f838e9774ffb126476b812edbaf22
Nnenna-udefi <miriamudefi@gmail.com>
2022-05-18 12:45:17 -0700

    Update member.js

--- src/components/Dashboard/Members/member.js
+++ src/components/Dashboard/Members/member.js
@@ -1,13 +1,57 @@
 import React from 'react'
 import Dashboard from '../../pages/dashboard'
+import {useState, useEffect} from 'react'
 
-const member = () => {
+const Member = () => {
+
+  const [member, setMember] = useState([])
+
+  useEffect(() => {
+    fetch('https://fakerapi.it/api/v1/persons?_quantity=10')
+  .then(response => response.json())
+  .then(data => setMember(data.data))
+
+
+  
+    
+  }, [])
+
+  console.log(member)
+  
+  
   return (
     <div className='main'>
         <Dashboard />
-   <p>gdfhgdhgfdhgfh</p>
+   
+        <table>
+          <thead>
+              <tr>
+                  <th>FULL NAME</th>
+                  <th>EMAIL</th>
+                  <th>PHONE NUMBER</th>
+                  <th>GENDER</th>
+              </tr>
+          </thead>
+          
+
+          <tbody>
+            {
+          
+              <tr >
+                <td>{member.name}</td>
+              <td>{member.email}</td>
+              <td>{member.phone}</td>
+              <td>{member.gender}</td>
+             
+            </tr>
+         
+
+            }
+            
+          </tbody>
+       </table>
     </div>
   )
 }
 
-export default member
+export default Member
